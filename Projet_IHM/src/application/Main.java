9a635974ea55b7a1beeb324943aeb79c2fbee1a3
qdecla <qdecla@scinfe059.univ-pau.fr>
2018-11-28 08:29:34 +0100

    création projet + création page acceuil
    Signed-off-by: qdecla <qdecla@scinfe059.univ-pau.fr>

---
+++ Projet_IHM/src/application/Main.java
@@ -0,0 +1,28 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.Parent;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("Page_acceuil_langue.fxml")); 
+			
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
