562364e6cb008371d6bbfa2572ad6889acacd858
Théo <Tho@192.168.1.13>
2018-12-12 19:15:50 +0100

    TIENS QUENTIN TU VOULAIS QUE JE COMMIT

--- Projet_IHM/src/Controller/Grammaire_DController.java
+++ Projet_IHM/src/Controller/Grammaire_DController.java
@@ -9,7 +9,6 @@ import java.util.ResourceBundle;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXRadioButton;
 
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 import javafx.scene.layout.Pane;
@@ -20,11 +19,11 @@ public class Grammaire_DController implements Initializable{
 	@FXML
 	private JFXRadioButton false1, false2, false3, false4, false5, false6, false7, false8;
 	@FXML
-	private Pane correc_q1, correc_q2, correc_q3, correc_q4, correc_q5, correc_q6, correc_q7, correc_q8;
+	private Pane correc_q2, correc_q5, correc_q7;
 	@FXML
-	private ImageView img_q1, img_q2, img_q3, img_q4, img_q5, img_q6, img_q7, img_q8;
+	private ImageView img_q1, img2_q1, img_q2, img2_q2, img_q3, img2_q3, img_q4, img2_q4, img_q5, img2_q5, img_q6, img2_q6, img_q7, img2_q7, img_q8, img2_q8;
 	@FXML
-	private JFXButton btn_valider;
+	private JFXButton btn_valider, btn_retour;
 	
 	public void true1_RDButton(){
 		true1.setSelected(true);
@@ -107,18 +106,101 @@ public class Grammaire_DController implements Initializable{
 	}
 	
 	public void check_answers() {
-		/*if((true1.isSelected() || false1.isSelected()) && (true2.isSelected() || false2.isSelected()) && (true3.isSelected() || false3.isSelected())
+		if((true1.isSelected() || false1.isSelected()) && (true2.isSelected() || false2.isSelected()) && (true3.isSelected() || false3.isSelected())
 			&& (true4.isSelected() || false4.isSelected()) && (true5.isSelected() || false5.isSelected()) && (true6.isSelected() || false6.isSelected())
-			&& (true7.isSelected() || false7.isSelected()) && (true8.isSelected() || false8.isSelected())){*/
-				if(true1.isSelected()) {
-					Image image = new Image("img/drapeau_anglais.png");
-					img_q1.setImage(image);
+			&& (true7.isSelected() || false7.isSelected()) && (true8.isSelected() || false8.isSelected())){
+				//Question 1
+				if(true1.isSelected()){
 					img_q1.setVisible(true);
+					img2_q1.setVisible(false);
 				}
-		/*}
+				else if(false1.isSelected()) {
+					img2_q1.setVisible(true);
+					img_q1.setVisible(false);
+				}
+				
+				//Question 2
+				if(true2.isSelected()){
+					img2_q2.setVisible(true);
+					img_q2.setVisible(false);
+				}
+				else if(false2.isSelected()) {
+					img_q2.setVisible(true);
+					img2_q2.setVisible(false);
+				}
+				
+				//Question 3
+				if(true3.isSelected()){
+					img_q3.setVisible(true);
+					img2_q3.setVisible(false);
+				}
+				else if(false3.isSelected()) {
+					img2_q3.setVisible(true);
+					img_q3.setVisible(false);
+				}
+				
+				//Question 4
+				if(true4.isSelected()){
+					img_q4.setVisible(true);
+					img2_q4.setVisible(false);
+				}
+				else if(false4.isSelected()) {
+					img2_q4.setVisible(true);
+					img_q4.setVisible(false);
+				}
+				
+				//Question 5
+				if(true5.isSelected()){
+					img2_q5.setVisible(true);
+					img_q5.setVisible(false);
+				}
+				else if(false5.isSelected()) {
+					img_q5.setVisible(true);
+					img2_q5.setVisible(false);
+				}
+				
+				//Question 6
+				if(true6.isSelected()){
+					img_q6.setVisible(true);
+					img2_q6.setVisible(false);
+				}
+				else if(false6.isSelected()) {
+					img2_q6.setVisible(true);
+					img_q6.setVisible(false);
+				}
+				
+				//Question 7
+				if(true7.isSelected()){
+					img2_q7.setVisible(true);
+					img_q7.setVisible(false);
+				}
+				else if(false7.isSelected()) {
+					img_q7.setVisible(true);
+					img2_q7.setVisible(false);
+				}
+				
+				//Question 8
+				if(true8.isSelected()){
+					img_q8.setVisible(true);
+					img2_q8.setVisible(false);
+				}
+				else if(false8.isSelected()) {
+					img2_q8.setVisible(true);
+					img_q8.setVisible(false);
+				}
+					
+				correc_q2.setVisible(true);
+				correc_q5.setVisible(true);
+				correc_q7.setVisible(true);
+				
+		}
 		else {
 			System.out.println("Vous n'avez pas répondu à toutes les questions !");
-		}*/
+		}
+	}
+	
+	public void back_btn() {
+		
 	}
 	
 	@Override
--- Projet_IHM/src/application/Main.java
+++ Projet_IHM/src/application/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("../FXML/Temps_et_Conjugaison_D.fxml")); 	
+			Parent root = FXMLLoader.load(getClass().getResource("../FXML/Menu_principal.fxml")); 	
 			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Menu Principal");
